563a5cebfbe472485e833595bae7e0ec354a6dfa
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-29 20:37:44 +0530

    initial commit

--- config/connectDB.js
+++ config/connectDB.js
@@ -12,34 +12,14 @@
         
 //     }
 // }
-
-
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+const connectDb=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL || "" );
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+    }
 }
-
-export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongooseInstance) => {
-      console.log("MongoDB connected ✅");
-      return mongooseInstance;
-    }).catch((err) => {
-      console.error("MongoDB connection error:", err);
-      throw err; // let the function fail gracefully
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+export default connectDb;
