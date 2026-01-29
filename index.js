a6f90b552c3ca772a4b3ca71efd180b823b6217e
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-29 20:34:02 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,81 @@
+// import express from 'express'
+// import cors from 'cors'
+// import cookieParser  from 'cookie-parser'
+// import dotenv from 'dotenv'
+// import { connectDB } from './config/connectDB.js'             //.js is imp because we are using import 
+// import userRoutes from './routes/user.routes.js'   //name can be anything here like. userRoutes
+// import sellerRoutes from './routes/seller.routes.js'
+// import productRoutes from './routes/product.routes.js'
+// import cartRoutes from './routes/cart.routes.js'
+// import orderRoutes from './routes/order.routes.js'
+// import addressRoutes from './routes/address.routes.js'
+// import { connectCloudinary } from './config/cloudinary.js'
+
+
+// dotenv.config()
+// const app=express()
+
+// await connectDB();
+// connectCloudinary()
+// const allowedOrigins=['http://localhost:5173','http://localhost:5174',
+//     // process.env.CLIENT_URL
+// ]
+
+// //middlewares
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(cookieParser())
+// app.use(cors({origin: allowedOrigins , credentials:true}));
+
+
+
+// //api end points
+// // app.use('/images', express.static("uploads"))  //uploads folder will create now here save images
+// app.use('/api/user' , userRoutes)
+// app.use('/api/seller',sellerRoutes)
+// app.use('/api/product',productRoutes)
+// app.use('/api/cart',cartRoutes)
+// app.use('/api/order',orderRoutes)
+// app.use('/api/address',addressRoutes)
+
+// app.get('/', (req, resp) => {
+//   try {
+//     resp.send("yes it is working");
+//   } catch (err) {
+//     console.error("GET / error:", err);
+//     resp.status(500).send("Server Error");
+//   }
+// });
+
+
+
+// const PORT=process.env.PORT || 4000;
+// // app.listen(PORT ,()=>{
+// //     console.log(`server is running on http://localhost:${PORT}`)
+// // })
+
+// export default app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import express from 'express'
 import cors from 'cors'
-import cookieParser  from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
-import { connectDB } from './config/connectDB.js'             //.js is imp because we are using import 
-import userRoutes from './routes/user.routes.js'   //name can be anything here like. userRoutes
+import { connectDB } from './config/connectDB.js'
+import userRoutes from './routes/user.routes.js'
 import sellerRoutes from './routes/seller.routes.js'
 import productRoutes from './routes/product.routes.js'
 import cartRoutes from './routes/cart.routes.js'
@@ -11,48 +83,43 @@ import orderRoutes from './routes/order.routes.js'
 import addressRoutes from './routes/address.routes.js'
 import { connectCloudinary } from './config/cloudinary.js'
 
-
 dotenv.config()
-const app=express()
+const app = express()
 
-await connectDB();
-connectCloudinary()
-const allowedOrigins=['http://localhost:5173','http://localhost:5174',
-    // process.env.CLIENT_URL
-]
-
-//middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser())
-app.use(cors({origin: allowedOrigins , credentials:true}));
-
-
-
-//api end points
-// app.use('/images', express.static("uploads"))  //uploads folder will create now here save images
-app.use('/api/user' , userRoutes)
-app.use('/api/seller',sellerRoutes)
-app.use('/api/product',productRoutes)
-app.use('/api/cart',cartRoutes)
-app.use('/api/order',orderRoutes)
-app.use('/api/address',addressRoutes)
-
-app.get('/', (req, resp) => {
+const startServer = async () => {
   try {
-    resp.send("yes it is working");
+    await connectDB()
+    connectCloudinary()
+
+    const allowedOrigins = ['http://localhost:5173','http://localhost:5174']
+
+    // middlewares
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser())
+    app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+    // API routes
+    app.use('/api/user', userRoutes)
+    app.use('/api/seller', sellerRoutes)
+    app.use('/api/product', productRoutes)
+    app.use('/api/cart', cartRoutes)
+    app.use('/api/order', orderRoutes)
+    app.use('/api/address', addressRoutes)
+
+    // test route
+    app.get('/', (req, res) => {
+      res.send("yes it is working")
+    })
+
+    const PORT = process.env.PORT || 4000
+    // Uncomment for local testing
+    // app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
   } catch (err) {
-    console.error("GET / error:", err);
-    resp.status(500).send("Server Error");
+    console.error("Server startup error:", err)
   }
-});
+}
 
-
-
-const PORT=process.env.PORT || 4000;
-// app.listen(PORT ,()=>{
-//     console.log(`server is running on http://localhost:${PORT}`)
-// })
+startServer()
 
 export default app
