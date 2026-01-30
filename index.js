7ab105eb9314c8d80bd2c5db08377e69ac35b174
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-30 14:07:43 +0530

    initial commit

--- index.js
+++ index.js
@@ -39,12 +39,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser())
 
-// app.use(cors({origin: allowedOrigins , credentials:true}));
+app.use(cors({origin: allowedOrigins , credentials:true}));
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}))
+// app.use(cors({
+//   origin: true,
+//   credentials: true
+// }))
 
 
 //api end points
