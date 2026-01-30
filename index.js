179cb73a4caac1feccbb50b191918fce34375724
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-30 12:59:35 +0530

    initial commit

--- index.js
+++ index.js
@@ -65,9 +65,5 @@ app.get('/', (req, resp) => {
 
 
 
-// const PORT=process.env.PORT || 4000;
-// app.listen(PORT ,()=>{
-//     console.log(`server is running on http://localhost:${PORT}`)
-// })
 
 export default app
