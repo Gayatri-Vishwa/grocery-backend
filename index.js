cedd64d00f3ccffe8a17f125e7e331021b513cd8
gayatriDev02 <blossomjasmine02@gmail.com>
2026-02-23 09:35:09 +0530

    added port for render

--- index.js
+++ index.js
@@ -2,86 +2,47 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import cors from 'cors'
-import cookieParser  from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import mongoose from "mongoose";
-import serverless from 'serverless-http';
-// import { connectDB } from './config/connectDB.js'             //.js is imp because we are using import 
-import userRoutes from './routes/user.routes.js'   //name can be anything here like. userRoutes
+
+import userRoutes from './routes/user.routes.js'
 import sellerRoutes from './routes/seller.routes.js'
 import productRoutes from './routes/product.routes.js'
 import cartRoutes from './routes/cart.routes.js'
 import orderRoutes from './routes/order.routes.js'
 import addressRoutes from './routes/address.routes.js'
-import { connectCloudinary } from './config/cloudinary.js'
-const app=express()
 
-//new
+const app = express()
+
 app.set("trust proxy", 1);
-// const PORT = process.env.PORT || 10000;
 
-
-
-
-//  connectDB();
-
- let isConnected=false
-async function connectDb() {
-     if (isConnected) return;
-    try{
-        await mongoose.connect(process.env.MONGO_URL ,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
-        isConnected=true;
-        console.log("Connected to mongodb");
-        
-    }
-    catch(error){
-        console.error("Error connected to mongo db", error);
-        
-    }
-}
-
-app.use(async (req, res, next) => {
-  if (!isConnected) await connectDb();
-  next();
-});
-
-
-//  await connectCloudinary();
-// app.use(async (req, res, next) => {
-//    await connectCloudinary();
-//   next();
-// });
-
-
-const allowedOrigins=[
-      
-      process.env.CLIENT_URL,
-]
-
-//middlewares
+// middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser())
 
-app.use(cors({origin: allowedOrigins , credentials:true}));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
+// routes
+app.use('/api/user', userRoutes)
+app.use('/api/seller', sellerRoutes)
+app.use('/api/product', productRoutes)
+app.use('/api/cart', cartRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/address', addressRoutes)
 
-//api end points
-// app.use('/images', express.static("uploads"))  //uploads folder will create now here save images
-app.use('/api/user' , userRoutes)
-app.use('/api/seller',sellerRoutes)
-app.use('/api/product',productRoutes)
-app.use('/api/cart',cartRoutes)
-app.use('/api/order',orderRoutes)
-app.use('/api/address',addressRoutes)
+// DB connect
+async function connectDb() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("MongoDB connected");
+}
 
+const PORT = process.env.PORT || 5000;
 
-
-
-
-
-export default app
-
+app.listen(PORT, async () => {
+  await connectDb();
+  console.log(`Server running on port ${PORT}`);
+});
