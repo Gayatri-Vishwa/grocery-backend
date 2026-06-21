cd72e3e90e5b21a614f45e39cc6809da0aaf3f1d
gayatriDev02 <blossomjasmine02@gmail.com>
2026-06-21 22:33:07 +0530

    addproduct  change

--- config/multer.js
+++ config/multer.js
@@ -33,11 +33,3 @@ export default upload;
 
 
 
-// const storage = multer.diskStorage({
-//   destination: "uploads/",
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage });
